ea3e0f8695776e9c5bdd05b664ec1ea593594183
chen <151507670@qq.com>
2019-02-13 22:27:12 +0800

    优化

--- app/src/main/java/com/inappupdate/MainActivity.java
+++ app/src/main/java/com/inappupdate/MainActivity.java
@@ -1,11 +1,11 @@
 package com.inappupdate;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
@@ -13,10 +13,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
